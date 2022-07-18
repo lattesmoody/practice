3c787e563586bee7c7b8e701d1c3025449ac446d
lattesmoody <lattesmoody@gmail.com>
2022-07-19 01:41:13 +0900

    complete codeup_1050.c

--- codeup_1050.c
+++ codeup_1050.c
@@ -3,10 +3,9 @@
 int main(){
     int a, b;
     scanf("%d %d", &a, &b);
-    if (a > b){
+    if(a == b){
         printf("%d", 1);
-    }
-    else{
+    } else{
         printf("%d", 0);
     }
     return 0;
