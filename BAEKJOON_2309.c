966e0837777473c6d087897865633c7a574cbb88
lattesmoody <lattesmoody@gmail.com>
2022-07-26 00:51:06 +0900

    failed baekjoon_2309

---
+++ BAEKJOON_2309.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a[9];
+    int nan1, nan2,nan3,nan4,nan5,nan6,nan7,nan8,nan9;
+    scanf("%d %d %d %d %d %d %d %d %d", &a[0],&a[1],&a[2],&a[3],&a[4],&a[5],&a[6],&a[7],&a[8],&a[9]);
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n", nan1,nan2,nan3,nan4,nan5,nan6,nan7,nan8,nan9);
+}
+
+// 내가 봐도 정말 코드 짤 줄 모르는 것 같음.
+// 남이 만든 코드를 보고 해석한 다음 내 것으로 만드는 연습을 해야겠다..
