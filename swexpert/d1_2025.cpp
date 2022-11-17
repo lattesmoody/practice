f7e20299d1ce6035baafa544747898f1cced3ec4
lattesmoody <lattesmoody@gmail.com>
2022-11-17 20:01:18 +0900

    complete d1_2025

---
+++ swexpert/d1_2025.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    int b;
+    for (int i = 0; i <= T; i++)
+    {
+        b = b + i;
+    }
+    cout << b << " ";
+    return 0;
+}
